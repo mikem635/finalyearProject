d52816d9741e6358bf1d9babe8692389c50bfaf5
mikem635 <michaelmurphy@Mac-Pro.local>
2018-03-08 09:05:51 +0000

    More models work

--- ecommerce/src/events/admin.py
+++ ecommerce/src/events/admin.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Event
+
+admin.site.register(Event)
+
 # Register your models here.
--- ecommerce/src/events/models.py
+++ ecommerce/src/events/models.py
@@ -6,5 +6,5 @@ from django.db import models
 class Event(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-    price = models.DecimalField(decimal_places=2, max_digits=4)
+    price = models.DecimalField(decimal_places=2, max_digits=4, default=25.00)
 # Create your models here.
