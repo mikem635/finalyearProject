7de25c7162f0174864cdff7af83ed31fabc7371b
mikem635 <michaelmurphy@Mac-Pro.local>
2018-03-13 08:03:21 +0000

    image field
    add

--- ecommerce/src/events/models.py
+++ ecommerce/src/events/models.py
@@ -2,11 +2,29 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import random
+import os
+
+def get_ext(filename):
+    basename = os.path.basename(filename)
+    name, ext = os.path.splitext(basename)
+    return name, ext
+
+#to remove white spave from a file uploaded
+def upload_image_path(instance, filename):
+    print(instance)
+    print(filename)
+    new_filename = random.randint(1,5000000)
+    name, ext = get_ext(filename)
+    new_file = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "events/{new_filename}/{new_file}".format(new_filename=new_filename,
+                                                        new_file=new_file)
 
 class Event(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     price = models.DecimalField(decimal_places=2, max_digits=4, default=25.00)
+    image = models.ImageField(upload_to=upload_image_path, blank=True, null=True)
     SEFS = 'SEFS'
     Medicine = 'Medicine'
     BusinessAndLaw = 'Business And Law'
@@ -19,7 +37,7 @@ class Event(models.Model):
         (ARTS, 'Arts'),
         (ALL, 'All')
     )
-    collegeOnSalleTo = models.CharField(max_length=2,
+    collegeOnSalleTo = models.CharField(max_length=20,
                                       choices=collegeOnSalleTo_choices,
                                       default=SEFS)
 
